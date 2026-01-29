e015774145b2692abf6f2071a261320a4a49aa02
tara53cic <tara53cicc@gmail.com>
2026-01-29 11:52:53 +0100

    feat: G-Counter implemented

--- src/main/java/isa/jutjubic/service/VideoPostService.java
+++ src/main/java/isa/jutjubic/service/VideoPostService.java
@@ -1,14 +1,19 @@
 package isa.jutjubic.service;
 
+import isa.jutjubic.crdt.GCounter;
 import isa.jutjubic.dto.VideoPostCreateRequest;
 import isa.jutjubic.model.User;
 import isa.jutjubic.model.VideoPost;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public interface VideoPostService {
+
     VideoPost createPost(VideoPostCreateRequest request, MultipartFile thumbnail, MultipartFile video, User author) throws IOException;
 
     List<VideoPost> getVideos();
@@ -16,4 +21,10 @@ public interface VideoPostService {
     VideoPost getVideoById(Long id);
 
     byte[] getThumbnail(Long id) throws IOException;
+
+    void recordView(Long id);
+    GCounter getCounter(Long videoId);
+    long getLocalViewCount(Long videoId);
+    void merge(Long videoId, Map<String, Long> otherState);
+    void persistViewsSnapshot(Long videoId);
 }
